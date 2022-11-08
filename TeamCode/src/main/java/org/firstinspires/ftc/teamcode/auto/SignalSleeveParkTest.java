d79e3ef4a48602f1c8658f73aaa2ad35515733a8
Junle Yan <yanjunlereal@gmail.com>
2022-11-07 16:15:45 -1000

    Added auto test script

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/SignalSleeveParkTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/SignalSleeveParkTest.java
@@ -9,10 +9,8 @@ import org.firstinspires.ftc.teamcode.subsystem.SensorGroup;
 
 
 @Autonomous(name="Signal Sleeve Based Parking", group="Auto")
-public class SignalSleevePark extends LinearOpMode {
+public class SignalSleeveParkTest extends LinearOpMode {
 
-
-    private MecanumDrive drive = new MecanumDrive();
     private SensorGroup sensor = new SensorGroup();
     private ElapsedTime runtime = new ElapsedTime();
     private int scheduled_zone;
@@ -20,36 +18,30 @@ public class SignalSleevePark extends LinearOpMode {
 
     @Override
     public void runOpMode() {
-        drive.init(hardwareMap);
         sensor.init(hardwareMap);
         waitForStart();
 
-        drive.setNormal(0.3,0.3);
         while (opModeIsActive()){
             telemetry.addData("Task","Moving forward until close to sleeve");
             if (!(sensor.getZone() == 0)) {
                 scheduled_zone = sensor.getZone();
             }
         }
-        drive.stop();
 
 
         runtime.reset();
         while ((scheduled_zone == 1) && (runtime.milliseconds() < 3000)) {
-            drive.setStrafe(-0.3, -0.3);
             telemetry.addData("Task","Moving toward the left");
         }
-        drive.stop();
 
         runtime.reset();
         while ((scheduled_zone == 3) && (runtime.milliseconds() < 3000)) {
-            drive.setStrafe(0.3, 0.3);
             telemetry.addData("Task","Moving forward the right");
         }
-        drive.stop();
 
 
         telemetry.addData("Task","Completed");
+        telemetry.addData("Route", scheduled_zone);
     }
 
 
